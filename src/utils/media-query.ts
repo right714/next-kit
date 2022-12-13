238ea0f4b1e2eb00a5f7d468036e4e8ae1d3095e
right714 <maru39sankaku@gmail.com>
2022-12-13 17:20:44 +0900

    testが0件だとプッシュできないので、in-source testを追加

--- src/utils/media-query.ts
+++ src/utils/media-query.ts
@@ -4,4 +4,13 @@ const mq: { [key: string]: string } = {
   desktop: `@media (min-width: 1024px)`,
 };
 
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  it("Media Query", () => {
+    expect(mq.tablet).toBe("@media (min-width: 600px)");
+    expect(mq.desktop).toBe("@media (min-width: 1024px)");
+  });
+}
+
 export { mq };
